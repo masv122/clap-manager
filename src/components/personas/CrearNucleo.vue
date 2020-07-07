75b83a004cac604a1bd86c101b07f13d4a0c1763
Marcel <masv122@gmail.com>
2020-07-07 09:46:37 -0400

    ci(validaciones): validaciones agregadas

--- src/components/personas/CrearNucleo.vue
+++ src/components/personas/CrearNucleo.vue
@@ -1,82 +1,145 @@
 <template>
   <div class="q-gutter-md">
-    <q-form @submit.prevent class="q-gutter-md shadow-1 round q-px-md q-pb-md">
-      <div class="text-h5 q-ml-xs q-pt-md">
-        <q-icon name="group" />Crear nuevo nucleo
-      </div>
-      <q-separator color="negative" inset />
-      <q-input
-        label-color="negative"
-        clearable
-        color="negative"
-        v-model="text"
-        type="text"
-        label="Nombre"
+    <div class="text-h5 q-ml-xs q-pt-md">
+      <q-icon name="group" />Crear nuevo nucleo
+    </div>
+    <q-separator color="negative" inset />
+    <q-input
+      label-color="negative"
+      clearable
+      color="negative"
+      v-model="$v._nombreNucleo.$model"
+      error-message="Debe proporcionar un nombre para el nucleo"
+      :error="$v._nombreNucleo.$invalid"
+      type="text"
+      label="Nombre"
+    />
+    <q-input
+      label-color="negative"
+      clearable
+      color="negative"
+      v-model="$v._direccion.$model"
+      error-message="Debe proporcionar un nombre para el nucleo"
+      :error="$v._direccion.$invalid"
+      type="text"
+      label="Nombre"
+    />
+    <div class="text-h5 q-ml-xs q-pt-xs">
+      <q-icon name="place" />Asignacion de sector
+    </div>
+    <q-separator color="negative" inset />
+    <q-select
+      label="Sector"
+      error-message="Debe seleccionar un sector al nucleo"
+      :error="$v._sector.$invalid"
+      use-input
+      behavior="menu"
+      hide-selected
+      fill-input
+      lazy-rules
+      :rules="[ val => val !== null && val !== '' || 'Seleccione un sector']"
+      input-debounce="0"
+      :options="opciones"
+      @filter="filterFn"
+      style="padding-bottom: 32px"
+      label-color="negative"
+      color="negative"
+      v-model="$v._sector.$model"
+      option-value="id"
+      option-label="nombre"
+      emit-value
+      map-options
+    >
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">No results</q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+    <transition name="fade">
+      <q-chip
+        v-if="getInvalid"
+        color="red"
+        text-color="white"
+        icon="warning"
+        label="Debe llenar todos los campos correctamente"
+        class="q-mx-auto"
       />
-      <div class="text-h5 q-ml-xs q-pt-xs">
-        <q-icon name="place" />Asignacion de sector
-      </div>
-      <q-separator color="negative" inset />
-      <q-select
-        label="Sector"
-        :value="model"
-        use-input
-        behavior="menu"
-        hide-selected
-        fill-input
-        lazy-rules
-        :rules="[ val => val !== null && val !== '' || 'Seleccione un sector']"
-        input-debounce="0"
-        :options="options"
-        @filter="filterFn"
-        @input-value="setModel"
-        style="padding-bottom: 32px"
-      >
-        <template v-slot:no-option>
-          <q-item>
-            <q-item-section class="text-grey">No results</q-item-section>
-          </q-item>
-        </template>
-      </q-select>
-    </q-form>
+    </transition>
   </div>
 </template>
 
 <script>
-const stringOptions = [
-  "Google",
-  "Facebook",
-  "Twitter",
-  "Apple",
-  "Oracle"
-].reduce((acc, opt) => {
-  for (let i = 1; i <= 5; i++) {
-    acc.push(opt + " " + i);
-  }
-  return acc;
-}, []);
+import { mapGetters, mapMutations } from "vuex";
+import { required } from "vuelidate/lib/validators";
 export default {
   name: "AsignarNucleo",
   data() {
     return {
-      model: null,
-      stringOptions,
-      options: stringOptions,
-      text: ""
+      opciones: []
     };
   },
+  validations: {
+    _nombreNucleo: {
+      required
+    },
+    _sector: {
+      required
+    },
+    _direccion: {
+      required
+    }
+  },
+  computed: {
+    ...mapGetters("sectores", ["sectores", "sector"]),
+    ...mapGetters("personas", ["nombreNucleo", "direccion"]),
+    _sector: {
+      get() {
+        return this.sector;
+      },
+      set(value) {
+        this.updateSector(value);
+      }
+    },
+    _nombreNucleo: {
+      get() {
+        return this.nombreNucleo;
+      },
+      set(value) {
+        this.updateNombreNucleo(value);
+      }
+    },
+    _direccion: {
+      get() {
+        return this.direccion;
+      },
+      set(value) {
+        this.updateDireccion(value);
+      }
+    },
+    getInvalid() {
+      this.updateDatosNucleoInvalidos(this.$v.$invalid);
+      return this.$v.$invalid;
+    }
+  },
   methods: {
+    ...mapMutations("sectores", ["updateSector"]),
+    ...mapMutations("personas", [
+      "updateNombreNucleo",
+      "updateDatosNucleoInvalidos",
+      "updateDireccion"
+    ]),
     filterFn(val, update, abort) {
       update(() => {
         const needle = val.toLocaleLowerCase();
-        this.options = stringOptions.filter(
-          v => v.toLocaleLowerCase().indexOf(needle) > -1
+        this.opciones = this.sectores.filter(
+          v => v.nombre.toLocaleLowerCase().indexOf(needle) > -1
         );
       });
-    },
-    setModel(val) {
-      this.model = val;
     }
+  },
+  created() {
+    this.opciones = this.sectores;
   }
 };
 </script>
