e975dd62c2d4bc772749813c037ccfce150dd4d8
Marcel <masv122@gmail.com>
2020-07-15 17:32:36 -0400

    refactor(opciones): opciones de edicion de personas movidas a la tabla

--- src/components/personas/NavPersonas.vue
+++ src/components/personas/NavPersonas.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fit row q-gutter-y-md">
-    <q-toolbar class="col-lg-auto">
+    <q-toolbar class="col-6">
       <q-tabs v-model="tab" class="q-mx-auto text-negative">
         <q-route-tab
           :to="{ name: 'integrantes'}"
@@ -17,18 +17,10 @@
         />
       </q-tabs>
     </q-toolbar>
-    <q-toolbar class="col-lg-auto">
+    <q-toolbar class="col-6">
       <q-btn-group push rounded class="q-mx-auto">
         <q-btn color="positive" push @click="updateAgregarPersona" icon="add" label="Agregar" />
         <q-btn color="info" push @click="updateDetallesPersona" icon="article" label="Detalles" />
-        <q-btn color="amber" push @click="updateModificarPersona" icon="edit" label="Modificar" />
-        <q-btn
-          color="negative"
-          push
-          @click="updateConfirmacionEliminacion"
-          icon="delete"
-          label="Eliminar"
-        />
         <q-btn color="primary" push icon="print" label="Imprimir" />
       </q-btn-group>
     </q-toolbar>
@@ -50,7 +42,6 @@ export default {
       "updateModificarPersona",
       "updateDetallesPersona"
     ]),
-    ...mapMutations("global", ["updateConfirmacionEliminacion"])
   }
 };
 </script>
