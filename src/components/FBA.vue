2fe776d2196ec3663ec821d3004b35482a795fcd
Marcel <masv122@gmail.com>
2020-07-15 17:19:35 -0400

    feat(gui): agregado FBA en moviles

---
+++ src/components/FBA.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page-sticky position="bottom-right" :offset="[18, 18]" v-if="$q.screen.lt.sm">
+    <q-fab color="negative" text-color="white" icon="keyboard_arrow_left" direction="left">
+      <q-fab-action color="primary" text-color="white" icon="print" />
+      <q-fab-action color="info" text-color="white" icon="article" />
+      <q-fab-action color="positive" text-color="white" @click="agregar" icon="add" />
+    </q-fab>
+  </q-page-sticky>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  name: "FBA",
+  methods: {
+    ...mapMutations("sectores", ["updateAgregar"]),
+    ...mapMutations("personas", ["updateAgregarPersona"]),
+    ...mapMutations("pagos", ["updateAgregarPago"]),
+    agregar() {
+      if (this.$route.path.includes("sectores")) this.updateAgregar();
+      else if (this.$route.path.includes("personas"))
+        this.updateAgregarPersona();
+      else if (this.$route.path.includes("pagos")) this.updateAgregarPago();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
