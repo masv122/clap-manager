1deb689cbe435fb16f8ef4384f79526b897846cd
Marcel <masv122@gmail.com>
2020-07-23 12:38:10 -0400

    ci(metodo): metodo obtener nucleo implementado en pago

--- src/class/pago.js
+++ src/class/pago.js
@@ -1,10 +1,38 @@
-export default class Pago {
-  constructor(monto, referencia, banco, estado, fecha, nucleo) {
+import Base from "./base";
+import { db } from "boot/pouchdb/index";
+import Nucleo from "./nucleo";
+
+export default class Pago extends Base{
+  constructor(monto, banco, estado, fecha, nucleo, id, rev) {
+    super(id, rev)
     this.monto = monto;
-    this.referencia = referencia;
     this.banco = banco;
     this.estado = estado;
     this.fecha = fecha;
     this.nucleo = nucleo;
+  }
+  async getNucleo() {
+    try {
+      const resultado = await db.local.rel.find("nucleo");
+      const indice = resultado.nucleos.findIndex(
+        nucleo => nucleo.id === this.nucleo
+      );
+      if (indice >= 0) {
+        const nucleo = new Nucleo(
+          resultado.nucleos[indice].cedula,
+          resultado.nucleos[indice].nombre,
+          resultado.nucleos[indice].direccion,
+          resultado.nucleos[indice].sector,
+          resultado.nucleos[indice].integrantes,
+          resultado.nucleos[indice].pagos,
+          resultado.nucleos[indice].id,
+          resultado.nucleos[indice].rev
+        );
+        return nucleo;
+      } else return null;
+    } catch (error) {
+      alert("error al obtener el nucleo 101: " + error);
+      return null;
+    }
   }
 }
