2a592faf7b15c61833a92237ef015c728dfe7a5f
Marcel <masv122@gmail.com>
2020-07-25 22:08:48 -0400

    refactor(ui): interfaz de informacion cambiada

--- src/components/personas/InformacionJefe.vue
+++ src/components/personas/InformacionJefe.vue
@@ -1,26 +1,46 @@
 <template>
-  <div>
-    <q-field borderless label="Nombre" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ nombre }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Cedula" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ cedula }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Codigo" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ codigo }}</div>
-      </template>
-    </q-field>
-  </div>
+  <q-card class="my-card">
+    <q-card-section>
+      <q-btn
+        fab
+        color="negative"
+        icon="supervised_user_circle"
+        class="absolute"
+        style="top: 0; right: 12px; transform: translateY(-50%);"
+      />
+
+      <div class="row no-wrap items-center">
+        <div class="col text-h6 ellipsis">Jefe de calle</div>
+        <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"></div>
+      </div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Nombre</div>
+      <div class="text-caption text-grey">{{ nombre }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Cedula</div>
+      <div class="text-caption text-grey">{{ cedula }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Codigo</div>
+      <div class="text-caption text-grey">{{ codigo }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Direccion</div>
+      <div class="text-caption text-grey">{{ direccion }}</div>
+    </q-card-section>
+    <q-card-actions align="right">
+      <q-btn flat round icon="launch" color="primary" @click="detallesPersona"/>
+    </q-card-actions>
+  </q-card>
 </template>
 
 <script>
+import redireccionamientoCRUD from "src/mixins/redireccionamientoCRUD";
 export default {
   name: "InformacionJefe",
+    mixins: [ redireccionamientoCRUD],
   props: {
     nombre: {
       type: String,
@@ -31,6 +51,10 @@ export default {
       default: null
     },
     codigo: {
+      type: String,
+      default: ""
+    },
+    direccion: {
       type: String,
       default: ""
     }
--- src/components/personas/InformacionPersonal.vue
+++ src/components/personas/InformacionPersonal.vue
@@ -1,31 +1,49 @@
 <template>
-  <div>
-    <q-field borderless label="Nombre" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ nombre }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Apellidos" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ apellido }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Cedula" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ cedula }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Telefono" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ telefono }}</div>
-      </template>
-    </q-field>
-  </div>
+  <q-card class="my-card">
+    <q-card-section>
+      <q-btn
+        fab
+        color="negative"
+        icon="info"
+        class="absolute"
+        style="top: 0; right: 12px; transform: translateY(-50%);"
+      />
+
+      <div class="row no-wrap items-center">
+        <div class="col text-h6 ellipsis">Datos del Integrante</div>
+        <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"></div>
+      </div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Nombre</div>
+      <div class="text-caption text-grey">{{ nombre }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Apellidos</div>
+      <div class="text-caption text-grey">{{ apellido }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Cedula</div>
+      <div class="text-caption text-grey">{{ cedula }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Telefono</div>
+      <div class="text-caption text-grey">{{ telefono }}</div>
+    </q-card-section>
+    <q-card-actions align="right">
+      <q-btn flat round icon="edit" color="warning" @click="modificarPersona" />
+      <q-btn flat round icon="delete" color="red" @click="eliminar" />
+    </q-card-actions>
+  </q-card>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import eliminarClase from "src/mixins/eliminarClase";
+import redireccionamientoCRUD from "src/mixins/redireccionamientoCRUD";
 export default {
   name: "InformacionPersonal",
+  mixins: [eliminarClase, redireccionamientoCRUD],
   props: {
     nombre: {
       type: String,
@@ -42,6 +60,15 @@ export default {
     telefono: {
       type: String,
       default: ""
+    }
+  },
+  computed: {
+    ...mapGetters("personas", ["persona"])
+  },
+  methods: {
+    eliminar() {
+      this.elminarSector();
+      this.$router.push({ name: "sectores" });
     }
   }
 };
