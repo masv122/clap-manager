68468e8ce41bc0c9b393667f0e0ee83195ce5203
Marcel <masv122@gmail.com>
2020-07-25 22:10:09 -0400

    refacto(ui): nuevo estilo ui

---
+++ src/components/sectores/InformacionDetallada.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-card class="my-card">
+    <q-card-section>
+      <q-btn
+        fab
+        color="negative"
+        icon="analytics"
+        class="absolute"
+        style="top: 0; right: 12px; transform: translateY(-50%);"
+      />
+
+      <div class="row no-wrap items-center">
+        <div class="col text-h6 ellipsis">Informacion detallada</div>
+        <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"></div>
+      </div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Nucleos</div>
+      <div class="text-caption text-grey">{{ nucleos.length }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Integrantes</div>
+      <div class="text-caption text-grey">{{ integrantes.length }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Pagos</div>
+      <div class="text-caption text-grey">{{ pagos.length }}</div>
+    </q-card-section>
+    <q-tabs v-model="tab" class="text-teal">
+      <q-tab class="text-primary" icon="perm_identity" name="integrantes" />
+      <q-tab class="text-secondary" icon="group" name="nucleos" />
+      <q-tab class="text-info" icon="payments" name="pagos" />
+    </q-tabs>
+    <q-separator />
+    <q-tab-panels v-model="tab" animated>
+      <q-tab-panel name="integrantes">
+        <tabla-integrantes :data="integrantes" />
+      </q-tab-panel>
+      <q-tab-panel name="nucleos">
+        <tabla-nucleos :data="nucleos" />
+      </q-tab-panel>
+      <q-tab-panel name="pagos">
+        <tabla-pagos :data="pagos" />
+      </q-tab-panel>
+    </q-tab-panels>
+  </q-card>
+</template>
+
+<script>
+import TablaIntegrantes from "components/personas/TablaIntegrantes.vue";
+import TablaNucleos from "components/personas/TablaNucleos.vue";
+import TablaPagos from "components/pagos/TablaPagos.vue";
+export default {
+  name: "InformacionDetallada",
+  props: {
+    nucleos: {
+      type: Array,
+      default: () => []
+    },
+    integrantes: {
+      type: Array,
+      default: () => []
+    },
+    pagos: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    TablaIntegrantes,
+    TablaNucleos,
+    TablaPagos
+  },
+  data() {
+    return {
+      tab: "one"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/sectores/InformacionSector.vue
+++ src/components/sectores/InformacionSector.vue
@@ -1,31 +1,49 @@
 <template>
-  <div>
-    <q-field borderless label="Nombre" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ nombre }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Estado" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ estado }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Municipio" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ municipio }}</div>
-      </template>
-    </q-field>
-    <q-field borderless label="Parroquia" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{ parroquia }}</div>
-      </template>
-    </q-field>
-  </div>
+  <q-card class="my-card">
+    <q-card-section>
+      <q-btn
+        fab
+        color="negative"
+        icon="info"
+        class="absolute"
+        style="top: 0; right: 12px; transform: translateY(-50%);"
+      />
+
+      <div class="row no-wrap items-center">
+        <div class="col text-h6 ellipsis">Datos del sector</div>
+        <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"></div>
+      </div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Nombre</div>
+      <div class="text-caption text-grey">{{ nombre }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Estado</div>
+      <div class="text-caption text-grey">{{ estado }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Municipio</div>
+      <div class="text-caption text-grey">{{ municipio }}</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">Parroquia</div>
+      <div class="text-caption text-grey">{{ parroquia }}</div>
+    </q-card-section>
+    <q-card-actions align="right">
+      <q-btn flat round icon="edit" color="warning" @click="modificarSector" />
+      <q-btn flat round icon="delete" color="red" @click="eliminar" />
+    </q-card-actions>
+  </q-card>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import eliminarClase from "src/mixins/eliminarClase";
+import redireccionamientoCRUD from "src/mixins/redireccionamientoCRUD";
 export default {
   name: "InformacionSector",
+  mixins: [eliminarClase, redireccionamientoCRUD],
   props: {
     nombre: {
       type: String,
@@ -43,6 +61,15 @@ export default {
       type: String,
       default: ""
     }
+  },
+  computed: {
+    ...mapGetters("sectores", ["sector"])
+  },
+  methods: {
+    eliminar() {
+      this.elminarSector();
+      this.$router.push({ name: "sectores" });
+    },
   }
 };
 </script>
