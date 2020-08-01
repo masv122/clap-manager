2e88aaac4489f530ab95f039655bd87c20d30514
Marcel <masv122@gmail.com>
2020-08-01 11:58:12 -0400

    ci(pago): acceso a la ruta agregar pago en movil

--- src/components/FBA.vue
+++ src/components/FBA.vue
@@ -1,30 +1,44 @@
 <template>
-  <q-page-sticky position="bottom-right" :offset="[18, 50]" v-if="$q.screen.lt.sm">
-    <q-fab color="negative" text-color="white" icon="keyboard_arrow_left" direction="left">
+  <q-page-sticky
+    position="bottom-right"
+    :offset="[18, 50]"
+    v-if="$q.screen.lt.sm"
+  >
+    <q-fab
+      color="negative"
+      text-color="white"
+      icon="keyboard_arrow_left"
+      direction="left"
+    >
       <q-fab-action color="primary" text-color="white" icon="print" />
-      <q-fab-action color="positive" text-color="white" @click="agregar" icon="add" />
+      <q-fab-action
+        color="positive"
+        text-color="white"
+        @click="agregar"
+        icon="add"
+      />
     </q-fab>
   </q-page-sticky>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
-export default {
-  name: "FBA",
-  methods: {
-    ...mapMutations("sectores", ["updateAgregar"]),
-    ...mapMutations("personas", ["updateAgregarPersona"]),
-    ...mapMutations("pagos", ["updateAgregarPago"]),
-    agregar() {
-      if (this.$route.path.includes("sectores"))
-        this.$router.push({ name: "Agregar sector" });
-      else if (this.$route.path.includes("personas"))
-        this.updateAgregarPersona();
-      else if (this.$route.path.includes("pagos")) this.updateAgregarPago();
-    }
-  }
-};
+  import { mapMutations } from "vuex";
+  export default {
+    name: "FBA",
+    methods: {
+      ...mapMutations("sectores", ["updateAgregar"]),
+      ...mapMutations("personas", ["updateAgregarPersona"]),
+      ...mapMutations("pagos", ["updateAgregarPago"]),
+      agregar() {
+        if (this.$route.path.includes("sectores"))
+          this.$router.push({ name: "Agregar sector" });
+        else if (this.$route.path.includes("personas"))
+          this.$router.push({ name: "Agregar persona" });
+        else if (this.$route.path.includes("pagos"))
+          this.$router.push({ name: "Agregar pago" });
+      },
+    },
+  };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
